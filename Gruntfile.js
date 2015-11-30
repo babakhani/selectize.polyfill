512ed6e599c8b19341a453f1ea8999f8ba2f3140
reza babakhani <babakhani.reza@gmail.com>
2015-11-30 13:02:12 +0330

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,21 @@
 module.exports = function (grunt) {
-    var fileBanner = '/*\n  <%= pkg.name %> - v<%= pkg.version %> \n ' +//
-        ' Author: reza babakhani \n ' + //
-        'http://babakhani.github.io/selectize_polyfill/datepicker \n */\n'
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        copy: {
-            main: {
-                files: [
-                    {
-                        expand: true,
-                        src: ['src/css/<%= pkg.name %>.css'],
-                        dest: 'dist/<%= pkg.version %>/css/<%= pkg.name %>-<%= pkg.version %>.css'
-                    }
-                ]
-            }
-        }
-    });
-    grunt.loadNpmTasks('grunt-contrib-concat');
+    //var fileBanner = '/*\n  <%= pkg.name %> - v<%= pkg.version %> \n ' +//
+    //    ' Author: reza babakhani \n ' + //
+    //    'http://babakhani.github.io/selectize_polyfill/datepicker \n */\n'
+    //// Project configuration.
+    //grunt.initConfig({
+    //    pkg: grunt.file.readJSON('package.json'),
+    //    copy: {
+    //        main: {
+    //            files: [
+    //                {
+    //                    expand: true,
+    //                    src: ['src/css/<%= pkg.name %>.css'],
+    //                    dest: 'dist/<%= pkg.version %>/css/<%= pkg.name %>-<%= pkg.version %>.css'
+    //                }
+    //            ]
+    //        }
+    //    }
+    //});
+    //grunt.loadNpmTasks('grunt-contrib-concat');
 };
--- README.md
+++ README.md
@@ -5,3 +5,12 @@ https://github.com/babakhani/selectize.polyfill
 
 Polyfill for Selectize https://github.com/brianreavis/selectize.js
 Support IE 6 7 8
+
+Usage:
+    <!--[if IE 6]>
+    <link rel="stylesheet" href="selectize_polyfill/selectize.ie6.css"/>
+    <![endif]-->
+    <!--[if lt IE 9]>
+    <link rel="stylesheet" href="selectize_polyfill/selectize.ie678.css"/>
+    <script type="text/javascript" src="selectize_polyfill/selectize.crossbrowser.js"></script>
+    <![endif]-->
